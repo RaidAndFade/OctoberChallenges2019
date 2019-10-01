5edb2650b2a1eac29c261417c0d915902acace11
Devion Wilson <dwilson030@gmail.com>
2019-10-01 12:04:06 -0400

    Added yes in groovy

---
+++ Yes/yes.groovy
@@ -0,0 +1,16 @@
+// Dev Wilson Https://github.com/devichris https://theycallme.dev
+
+
+
+
+class yes {
+   static void main(String[] args) {
+      sting var = "yes";
+		
+      while(var=="yes") {
+         println("yes");
+         
+      }
+   }
+}
+
