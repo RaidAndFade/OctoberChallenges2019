ba64b295d5d79762356322a9f1285c222f7b2cff
Christopher Austin <chriswaustin@gmail.com>
2019-09-30 20:11:32 -0400

    Update yes.cs

--- Yes/yes.cs
+++ Yes/yes.cs
@@ -1,22 +1,11 @@
 // By chriswaustin - https://github.com/chriswaustin
 
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Yes
-{
-    class Yes
-    {
-        static void Main(string[] args)
-        {
-            while (true)
-            {
-                Console.WriteLine("Yes");
-            }
-        }
+class Yes {
+  static void Main() {
+    while (true) {
+        Console.WriteLine("Yes");
     }
+  }
 }
